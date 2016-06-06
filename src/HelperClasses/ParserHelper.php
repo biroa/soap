cb2deb18337291657f07d6669c2594ca2291f1c0
biroa <adam.biro@gmail.com>
2016-06-06 17:08:27 +0100

    check variables

--- src/HelperClasses/ParserHelper.php
+++ src/HelperClasses/ParserHelper.php
@@ -34,6 +34,7 @@ class ParserHelper implements ParserInterface
     }
 
     /**
+     * Criteria getter
      * @return array
      */
     public function getCriteria()
@@ -41,75 +42,105 @@ class ParserHelper implements ParserInterface
         return $this->criteria;
     }
 
+    /**
+     * Criteria setter
+     * @param $str
+     *
+     */
     public function setCriteria($str)
     {
         $this->stringToParse = $str;
     }
 
     /**
+     * Parsing the data and get min_bedrooms ans max_bedrooms from it.
+     *
      * @return object $this
      */
     public function getBedRoomRange()
     {
         preg_match($this->getBedRoomRangePattern, $this->stringToParse, $output_array);
-        $min_max_bedrooms = $this->splitAtOr($output_array[0]);
-        $this->criteria['min_bedrooms'] = (int)$min_max_bedrooms[0];
-        $this->criteria['max_bedrooms'] = (int)$min_max_bedrooms[1];
+        if(!empty($output_array[0])) {
+            $min_max_bedrooms = $this->splitAtOr($output_array[0]);
+            $this->criteria['min_bedrooms'] = (int)$min_max_bedrooms[0];
+            $this->criteria['max_bedrooms'] = (int)$min_max_bedrooms[1];
+        }
 
         return $this;
     }
 
     /**
+     * Get bedroom number from the string
+     *
      * @return object $this
      */
     public function getBedRoomNum()
     {
         preg_match($this->getBedRoomNumPattern, $this->stringToParse, $output_array);
-        $this->criteria['bedroom'] = $this->getNumber($output_array[0]);
+        if(!empty($output_array[0])) {
+            $this->criteria['bedroom'] = $this->getNumber($output_array[0]);
+        }
 
         return $this;
     }
 
     /**
+     * Get add_type from the string (rent|let|sale)
+     *
      * @return object $this
      */
     public function getAdType()
     {
         preg_match($this->getAdTypePattern, $this->stringToParse, $output_array);
-        $this->criteria['ad_type'] = $output_array[0];
+        if(!empty($output_array[0])){
+            $this->criteria['ad_type'] = $output_array[0];
+        }
 
         return $this;
     }
 
     /**
+     * Get Area ... We suppose it starts with one uppercase letter which is
+     * followed by many lowercase
+     *
      * @return object $this
      */
     public function getAreas()
     {
         preg_match($this->getAreasPattern, $this->stringToParse, $output_array);
-        $this->criteria['area_type'] = $output_array[0];
+        if(!empty($output_array[0])) {
+            $this->criteria['area_type'] = $output_array[0];
+        }
 
         return $this;
     }
 
     /**
+     * Get price from a the string
+     *
      * @return object $this
      */
     public function getPrice()
     {
         preg_match($this->getPricePattern, $this->stringToParse, $output_array);
-        $this->criteria['price'] = $this->getNumber($output_array[0]);
+        if(!empty($output_array[0])) {
+            $this->criteria['price'] = $this->getNumber($output_array[0]);
+        }
 
         return $this;
     }
 
     /**
+     * Get property type currently house|apartment
+     *
      * @return object $this
      */
     public function getPropertyType()
     {
         preg_match($this->getPropertyTypePattern, $this->stringToParse, $output_array);
-        $this->criteria['property_type'] = $output_array[0];
+        if(!empty($output_array[0])) {
+            $this->criteria['property_type'] = $output_array[0];
+        }
 
         return $this;
     }
